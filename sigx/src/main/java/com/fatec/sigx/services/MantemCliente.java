7a97441b1684578b4180245489f0844bf36fb889
almei <almei@LAPTOP-102TOTT6>
2022-07-15 09:07:12 -0300

    1 - implementacao da api web post - versao inicial

---
+++ sigx/src/main/java/com/fatec/sigx/services/MantemCliente.java
@@ -0,0 +1,16 @@
+package com.fatec.sigx.services;
+import java.util.List;
+import java.util.Optional;
+
+import com.fatec.sigx.model.Cliente;
+import com.fatec.sigx.model.Endereco;
+public interface MantemCliente {
+	List<Cliente> consultaTodos();
+	Optional<Cliente> consultaPorCpf(String cpf);
+	Optional<Cliente> consultaPorId(Long id);
+	Optional<Cliente> save(Cliente cliente);
+	void delete (Long id);
+	Optional<Cliente> altera ( Cliente cliente);
+	Endereco obtemEndereco(String cep);
+}
+
